fb72344c5b4b51623893974551f701a72ed72399
Pablo Moreno <ssm.pablo@gmail.com>
2023-02-10 02:53:28 +0100

    post pubsub publish message & dockerfile

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const { PubSub } = require('@google-cloud/pubsub')
+const pubsub = new PubSub()
+const app = express();
+require('dotenv').config()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+const port = process.env.PORT || 8080
+
+app.listen(port, () => {
+  console.log('listening on port ', port)
+})
+
+app.post('/', async (req, res) => {
+  try {
+    const data = req.body;
+    await publishPubSubMessage(data)
+    res.status(204).send()
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error)
+  }
+})
+
+async function publishPubSubMessage(data) {
+  const buffer = Buffer.from(JSON.stringify(data))
+  await pubsub.topic('topic-demo').publish(buffer)
+}
